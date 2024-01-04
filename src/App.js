35cac757dc9e00c20b02b6c8d6a9fe06b285530b
Ameed Omar <ameed_201010@hotmail.com>
2024-01-04 16:54:10 +0200

    base11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./Components/Header";
 import Welcoming from "./Components/Welcoming";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import Details from "./Pages/Details";
@@ -12,12 +12,12 @@ function App() {
   const { isFavOpen } = useFavourites();
   return (
     <FavouriteProvider>
-      <Router basename="/AmeedOmar-React1">
+      <BrowserRouter basename="/AmeedOmar-React1">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details" element={<Details />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </FavouriteProvider>
   );
 }
